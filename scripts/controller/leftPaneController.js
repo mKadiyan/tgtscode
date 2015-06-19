2c3fb9d8279810df6b56552d59401e2ad007b37c
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2015-06-19 14:19:01 +0530

    add dynamic loading of html for left pane click

--- scripts/controller/leftPaneController.js
+++ scripts/controller/leftPaneController.js
@@ -1,18 +1,43 @@
 	var mainControllers = angular.module('MainControllers', []);
-	mainControllers.controller('leftPaneController', function($scope) {
-		$('#leftpane div ul li').click(function(){
-			console.log('clicked on rsk eher ');
-		})
+	mainControllers.controller('leftPaneController',['$scope', '$http',
+		function($scope,$http) {
+			$scope.contents ;
+			$scope.topic ;
+			$scope.orderField = 'position';
+			console.log('I am here in leftPaneController');
 
-		$scope.contents ;
-		$scope.topic ;
-		$scope.orderField = 'position';
-		console.log('I am here in leftPaneController');
-		$scope.display = function(topic,subtopic)
-		{
+			$scope.display = function(topic,subtopic)
+			{
 
-			 $('#leftpane div ul li').removeClass('selected');
-			$('#'+subtopic).addClass('selected');
-			console.log('clicked on '+topic+'--'+subtopic);
-		};
-	});
+				$http({
+					method  : 'GET',
+					url     : 'pages/'+topic+'/'+subtopic+'.html',
+					timeout : 10000,
+					params  : {},  // Query Parameters (GET)
+					transformResponse : function(data) {
+						// string -> XML document object
+						return $.parseHTML(data);
+					}
+				}).success(function(data, status, headers, config) {
+					console.dir('Checknew '+ data[0]);
+					console.dir('status '+ status);
+					console.dir('headers '+ headers);
+					console.dir('config '+ config);
+					$("#middlepane").html(data[0]);
+					$('#leftpane div ul li').removeClass('selected');
+					$('#'+subtopic).addClass('selected');
+				});
+
+			};
+
+			function objToString (obj) {
+				var str = '';
+				for (var p in obj) {
+					if (obj.hasOwnProperty(p)) {
+						str += obj[p];
+					}
+				}
+				return str;
+			}
+
+	}]);
